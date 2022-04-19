72915159c07bac8284d5b39f56edf2e69a516d51
developer69 <decoder0104@gmail.com>
2022-04-20 05:46:36 +0800

    SCHOLARSHIP
    first commit

--- scholarship/project_scholarship/models.py
+++ scholarship/project_scholarship/models.py
@@ -1,9 +1,44 @@
+import email
+from pyexpat import model
 from django.db import models
 
 class extenduser(models.Model):
     STATUS = (
     ("PENDING", "PENDING"),
-    ("ENROLLED", "ENROLLED"),
+    ("FOR REVIEW", "FOR REVIEW"),
+    ("APPROVED", "APPROVED"),
     )
+    department = models.CharField(max_length=100)
+    school = models.CharField(max_length=100)
+    course = models.CharField(max_length=100)
+    year = models.CharField(max_length=100)
+    firstname = models.CharField(max_length=100)
+    middlename = models.CharField(max_length=100)
+    lastname = models.CharField(max_length=100)
+    extention = models.CharField(max_length=100)
+    birthday = models.DateField()
+    birthplace = models.CharField(max_length=100)
+    religion = models.CharField(max_length=100)
+    cellphone = models.IntegerField()
+    gender = models.CharField(max_length=10)
+    age =  models.DecimalField(max_digits=11, decimal_places=0, default='')
+    email = models.EmailField(max_length=254, null=True, unique=True)
+    civil = models.CharField(max_length=10)
+    unit = models.CharField(max_length=100)
+    street = models.CharField(max_length=100)
+    barangey = models.CharField(max_length=100)
+    municipality = models.CharField(max_length=100)
+    province = models.CharField(max_length=100)
+    fname = models.CharField(max_length=100)
+    fcontact =  models.DecimalField(max_digits=11, decimal_places=0, default='')
+    foccupation =  models.CharField(max_length=100)
+    mname = models.CharField(max_length=100)
+    mcontact = models.DecimalField(max_digits=11, decimal_places=0, default='')
+    moccupation = models.CharField(max_length=100)
+    gname = models.CharField(max_length=100)
+    gcontact =  models.DecimalField(max_digits=11, decimal_places=0, default='')
+    goccupation = models.CharField(max_length=100)
+    income = models.CharField(max_length=100)
+    image = models.ImageField(upload_to='images/', null=True, blank=True)
 
 # Create your models here.
--- scholarship/project_scholarship/views.py
+++ scholarship/project_scholarship/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import redirect
+from django.core.exceptions import ObjectDoesNotExist
+from django.conf import settings
+from django.contrib.auth import authenticate, login, logout
+
+from django.contrib.auth.models import User, auth
+from django.contrib.auth.decorators import login_required
+import mysql.connector as sql
+from django.contrib.auth.models import User, auth
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate
 
 # Create your views here.
 def index(request):
@@ -7,3 +18,12 @@ def index(request):
 
 def registration(request):
     return render(request, 'activities/registration.html')
+
+def register(request):
+    if request.method == 'POST':
+        try:
+            user = User.objects.get(username=request.POST['username'], email=request.POST['email'])
+            return render(request, 'activities/signup.html', {'error': 'User already exists'})
+        except:
+            department = request.POST.get('department')
+    return redirect('/registration/')
